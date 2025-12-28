a386e740022242aededbb77242f720ed95868e27
abdallah-mattour <nobody.mattour@outlook.com>
2025-12-28 16:28:36 +0200

    Initial commit

---
+++ src/main/java/com/springboot/washcarsuite/WashCarSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.washcarsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashCarSuiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WashCarSuiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/washcarsuite/WashCarSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.washcarsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashCarSuiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
